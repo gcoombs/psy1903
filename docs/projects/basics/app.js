c0ae4533ae1a0ea70987aa5184ce84db7b9891e5
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-08 12:39:52 -0400

    Week 2 practice code from videos

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,31 @@
-console.log('Hello World');
+let experiment = 'Stroop';
+// console.log(typeof experiment); // Expected: string
+// console.log(experiment.charAt(0)); // Expected: 'S'
+
+let welcomeMessage = `
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+Please complete the ` + experiment + ` experiment in a quiet and secluded space, free from distractions.
+`;
+console.log(welcomeMessage);
+
+let trialCountMax = 20;
+// console.log(typeof trialCountMax); // Expected: number
+// console.log(trialCountMax.charAt(0));
+
+// TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+// alert('Welcome to the ' + experiment + ' experiment!')
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+// console.log(halfWayCount); // Expected: 20
+
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 > 15); // Expected: false
