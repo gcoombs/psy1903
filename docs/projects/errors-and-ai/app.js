419ad497f20b6a17b8f44ce4dc21ceb82fe42531
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-15 17:33:46 -0400

    Week 2 pre-task set commits ("errors & AI" through "page updates & input")

---
+++ docs/projects/errors-and-ai/app.js
@@ -0,0 +1,31 @@
+// Uncaught ReferenceError: hello is not defined
+// console.log(hello);
+
+// Uncaught SyntaxError: missing ) after argument list
+// alert(Welcome to our experiment);
+
+// Uncaught SyntaxError: Unexpected identifier 'code'
+// alert([your code here]);
+
+// Operators & Expressions
+// Assignment operator: =
+// Concatenation operator: +
+// Arithmetic Operationrs: + - / * 
+//     % (remainder) 
+//     ++ (increment - will increase by one, e.g., age + 1)
+//     -- (decrement - will decrease by one)
+//     ** (exponent)
+// Comparison operators: > < >= <= == !=
+// Logical Operators: && (and) || (or)
+// Don't forget about order of operations, including parentheses
+
+// console.log(5 == 5 && 10 > 1); // true
+// console.log('red' == 'blue' || 1 < 10); // true
+// console.log(20 > 15 < 15); // true
+// console.log(12 % 2 == 0); // true
+// console.log('red' != 'green' || 'orange' == 'purple'); // true
+// console.log((10 >= 10) && (false == false) && (20 !== 19)); // true
+// console.log(true && true || false); // true
+
+
+}
