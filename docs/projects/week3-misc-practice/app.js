20b17a85ae04e77aa30e699f60444ea4f71aa692
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-16 11:52:56 -0400

    Age Propmt

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -1,17 +1,28 @@
-let num1 = Math.floor(Math.random() * 10) + 1;;
-let num2 = Math.floor(Math.random() * 10) + 1;;
-let correctAnswer = num1 + num2;
+// let num1 = Math.floor(Math.random() * 10) + 1;;
+// let num2 = Math.floor(Math.random() * 10) + 1;;
+// let correctAnswer = num1 + num2;
 
-let response = prompt("What is " + num1 + " + " + num2 + "?");
+// let response = prompt("What is " + num1 + " + " + num2 + "?");
 
-let feedback = '';
+// let feedback = '';
 
-if (response == correctAnswer) {
-    feedback = "You got it correct!";
-} else if (response == correctAnswer + 1 || response == correctAnswer - 1) {
-    feedback = "You almost got this correct.";
+// if (response == correctAnswer) {
+//     feedback = "You got it correct!";
+// } else if (response == correctAnswer + 1 || response == correctAnswer - 1) {
+//     feedback = "You almost got this correct.";
+// } else {
+//     feedback = "You got it incorrect.";
+// }
+
+// alert(feedback + ' The expected answer is ' + correctAnswer + '.');
+
+let age = prompt('How old are you?');
+if (age > 0 && age < 12) {
+    alert('Child');
+} else if (age >= 12 && age < 18) {
+    alert('Teenager');
+} else if (age >= 18) {
+    alert('Adult');
 } else {
-    feedback = "You got it incorrect.";
+    alert('Please refresh and enter your age.');
 }
-
-alert(feedback + ' The expected answer is ' + correctAnswer + '.');
