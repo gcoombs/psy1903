79a181539f3fa606b827f343b60b78aabbcb3767
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-17 15:25:26 -0400

    Input/Output Review Step 2

--- docs/projects/input-output-review/app.js
+++ docs/projects/input-output-review/app.js
@@ -1,6 +1,11 @@
 // Create variables to store references to elements on the page
 let form = document.getElementsByTagName('form')[0];
 let results = document.getElementById('results');
+// Generate a new variable we want to put back into the equation blank space in html
+let equation = document.getElementById('equation');
+
+// Want to check that this is doing anything before we actually do the equation
+equation.innerHTML = 'Hello World';
 
 // Listen for the form to be submitted
 form.addEventListener('submit', function (event) {
