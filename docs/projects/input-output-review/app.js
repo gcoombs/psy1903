5625878ff2cb2a738dd19888b5dddbcd25642a02
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-17 15:40:14 -0400

    Input/Output Review Step 4: Input via inline elements

--- docs/projects/input-output-review/app.js
+++ docs/projects/input-output-review/app.js
@@ -4,14 +4,20 @@ let results = document.getElementById('results');
 // Generate a new variable we want to put back into the equation blank space in html
 let equation = document.getElementById('equation');
 
+// Need to 
+let num1Output = document.getElementById('num1');
+let num2Output = document.getElementById('num2');
+
 // Now filling in code for the actual equation
 let num1 = Math.floor(Math.random() * 10) + 1;
 let num2 = Math.floor(Math.random() * 10) + 1;
-console.log(num1); // To confirm that numbers are being randomly generated; not necessary for "final" code
-console.log(num2); // To confirm that numbers are being randomly generated; not necessary for "final" code
 // Using concatenation, fill out the equation with the two randomly generated numbers (num1 & num2)
 // Because this is not in an eventListener, this will happen when the page first loads (not waiting for an action to execute)
-equation.innerHTML = 'What is ' + num1 + ' + ' + num2 + '?';
+// equation.innerHTML = 'What is ' + num1 + ' + ' + num2 + '?';
+
+// Instead of using concatenation to create a text string that is then input back into the html index, placing each randomly generated number there
+num1Output.innerHTML = num1;
+num2Output.innerHTML = num2;
 
 // Listen for the form to be submitted
 form.addEventListener('submit', function (event) {
