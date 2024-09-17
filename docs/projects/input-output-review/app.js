843550d5a5381bc154105d7995c42011cc356a2b
Garth Coombs <garthcoombs@fas.harvard.edu>
2024-09-17 15:18:57 -0400

    Input/Output Review Step 1

---
+++ docs/projects/input-output-review/app.js
@@ -0,0 +1,16 @@
+// Create variables to store references to elements on the page
+let form = document.getElementsByTagName('form')[0];
+let results = document.getElementById('results');
+
+// Listen for the form to be submitted
+form.addEventListener('submit', function (event) {
+
+    // Prevent the default form submission b
+    event.preventDefault();
+
+    // Collect the response
+    let response = form.elements['response'].value;
+
+    // Report the results
+    results.innerHTML = 'Hello ' + response + '!';
+});
